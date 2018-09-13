8b4dba2eb0ac61083e21db8358caa88a894c519a
qrafzv <qrafzv88@gmail.com>
2018-09-13 11:18:18 -0400

    init

---
+++ pnp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
+url=http://www.ontarioimmigration.ca/en/pnp/OI_PNPNEW.html
+tempdir=~/Library/Caches/scripts
+temp=$tempdir/pnp
+mkdir -p $tempdir
+
+modified=$(phantomjs ./pnp.js|grep -A2 'success'|xargs echo)
+if [[ -f "$temp" && "$modified" != "$(cat $temp)" && "$modified" != "" ]];then
+    notimsg=$(cat $temp)"=>"${modified#???????????????}  
+    echo $notimsg|terminal-notifier  -title 'Atten' -open $url
+    osascript ./email-notifier.scpt 
+#else
+   # echo $modified|terminal-notifier -title 'Calm Down' -open $url
+fi
+if [[ "$modified" != "" ]]; then
+printf %s "$modified" > $temp
+fi
